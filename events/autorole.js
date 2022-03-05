7180140a60919c9b505a2474c8cfb2bc3b78c4d8
noxx7 <68088454+noxx7@users.noreply.github.com>
2022-03-05 11:58:46 +0800

    Add files via upload

---
+++ events/autorole.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+const { util } = require("simple-youtube-api");
+const i18n = require('../util/i18n');
+
+module.exports = {
+    name: "autorole",
+    description: i18n.__("autorole.description"),
+    async execute(message, args) {
+        client.on('guildMemberAdd', member => {
+            member.roles.add(member.guild.roles.cache.find(c => c.name === 'welcome'))
+
+            const welcomeEmbed = new Discord.MessageEmbed()
+                .setColor('CYAN')
+                .setTitle('**' + member.user.username + '** joined the server **' + member.guild.memberCount + '** people')
+                .setImage(i18n.__('mai.jpg'))
+
+            member.guild.channels.cache.find(i => i.name === 'Selamat Datang').send(WelcomeEmbed)
+        })
+    }
+}
