2ec4c472afe6e647b987a8f6c09366baa4858057
noxx7 <68088454+noxx7@users.noreply.github.com>
2022-03-19 21:37:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -78,3 +78,21 @@ client.on("message", async (message) => {
     message.reply(i18n.__("common.errorCommand")).catch(console.error);
   }
 });
+
+
+client.on('guildMemberAdd', (member) => {
+
+    const member = message.mentions.members.first();
+
+    let welcomeEmbed = new MessageEmbed()
+       .setColor("RANDOM")
+       .setTitle("WELCOME!")
+       .setDescription(`${member.user} telah bergabung ke server!`)
+       .setTimestamp();
+  
+    member.guild.channels.cache.get("949450331052462090").send(welcomeEmbed);
+  
+    var role = member.guild.roles.cache.find(role => role.name == "membs")
+    member.roles.add(role);
+  
+  });
