4ff4e94a946bde7c81d4a35aa1734dc980063c61
noxx7 <68088454+noxx7@users.noreply.github.com>
2022-03-19 21:52:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -80,19 +80,3 @@ client.on("message", async (message) => {
 });
 
 
-client.on('guildMemberAdd', (member) => {
-
-    const member = message.mentions.members.first();
-
-    let welcomeEmbed = new MessageEmbed()
-       .setColor("RANDOM")
-       .setTitle("WELCOME!")
-       .setDescription(`${member.user} telah bergabung ke server!`)
-       .setTimestamp();
-  
-    member.guild.channels.cache.get("949450331052462090").send(welcomeEmbed);
-  
-    var role = member.guild.roles.cache.find(role => role.name == "membs")
-    member.roles.add(role);
-  
-  });
